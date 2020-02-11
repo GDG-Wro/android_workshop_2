082937014944e2ee95c88165e7cfdb2fab914ef2
aleksandra-majchrzak <aleksandra.a.majchrzak@gmail.com>
2020-02-11 23:43:28 +0100

    Final app state.

--- app/src/main/java/com/example/mycardapp/MainActivity.kt
+++ app/src/main/java/com/example/mycardapp/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.mycardapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main_constraint.*
 
 class MainActivity : AppCompatActivity() {
 
+    var numberOfTaps = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_linear)
+        setContentView(R.layout.activity_main_constraint)
+
+        //findViewById<TextView>(R.id.nameTextView).text = "Marian"
+
+        tapsTextView.text = getString(R.string.number_of_taps, numberOfTaps)
+
+        petDogButton.setOnClickListener {
+            Log.d(this::class.java.simpleName, "number of taps: $numberOfTaps")
+            numberOfTaps += 1
+
+            tapsTextView.text = getString(R.string.number_of_taps, numberOfTaps)
+        }
     }
 }

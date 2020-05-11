d3fd950e11497fb660ef18df0ffb7a2e23d40089
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2020-05-11 16:16:26 +0200

    Add simple Fragment

--- app/src/main/java/com/example/mycardapp/ActivityA.kt
+++ app/src/main/java/com/example/mycardapp/ActivityA.kt
@@ -1,31 +1,15 @@
 package com.example.mycardapp
 
-import android.net.Uri
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_a.*
-import kotlin.random.Random
+import androidx.fragment.app.commit
 
 class ActivityA : AppCompatActivity(R.layout.activity_a) {
 
-    private val pickImageLauncher =
-        registerForActivityResult(ActivityResultContracts.GetContent(), ::onItemPicked)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        label.text = "${Random.nextInt()}"
-    }
-
-    fun onOpenBButtonClick(view: View) {
-        pickImageLauncher.launch("image/*")
-    }
-
-    private fun onItemPicked(uri: Uri?) {
-        if (uri != null) {
-            findViewById<ImageView>(R.id.image).setImageURI(uri)
+        supportFragmentManager.commit {
+            replace(R.id.fragmentContainer, FragmentA())
         }
     }
 }
---
+++ app/src/main/java/com/example/mycardapp/FragmentA.kt
@@ -0,0 +1,8 @@
+package com.example.mycardapp
+
+import androidx.fragment.app.Fragment
+
+class FragmentA : Fragment(R.layout.fragment_a) {
+
+
+}

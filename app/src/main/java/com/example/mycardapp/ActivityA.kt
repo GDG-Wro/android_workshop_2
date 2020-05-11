ce5ae42522e21204f12bfca8208083ce964a644d
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2020-05-11 03:14:52 +0200

    Add intent action view

--- app/src/main/java/com/example/mycardapp/ActivityA.kt
+++ app/src/main/java/com/example/mycardapp/ActivityA.kt
@@ -1,13 +1,15 @@
 package com.example.mycardapp
 
 import android.content.Intent
+import android.net.Uri
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class ActivityA : AppCompatActivity(R.layout.activity_a) {
 
     fun onOpenBButtonClick(view: View) {
-        val intent = Intent("com.example.show.b")
+        val intent = Intent(Intent.ACTION_VIEW)
+            .setData(Uri.parse("https://google.pl"))
         startActivity(intent)
     }
 }

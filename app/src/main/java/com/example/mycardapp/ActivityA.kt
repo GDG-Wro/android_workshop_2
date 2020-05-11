27de9968b1ce5495d63bf089bb49e56fd727762d
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2020-05-11 15:36:03 +0200

    Add saved state demo for onActivityResult

--- app/src/main/java/com/example/mycardapp/ActivityA.kt
+++ app/src/main/java/com/example/mycardapp/ActivityA.kt
@@ -1,15 +1,23 @@
 package com.example.mycardapp
 
 import android.net.Uri
+import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_a.*
+import kotlin.random.Random
 
 class ActivityA : AppCompatActivity(R.layout.activity_a) {
 
     private val pickImageLauncher =
         registerForActivityResult(ActivityResultContracts.GetContent(), ::onItemPicked)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        label.text = "${Random.nextInt()}"
+    }
 
     fun onOpenBButtonClick(view: View) {
         pickImageLauncher.launch("image/*")

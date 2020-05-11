7e4f03c11f3599629e031d1fd58feece77e7a87a
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2020-05-11 02:24:54 +0200

    Add Activity B

--- app/src/main/java/com/example/mycardapp/ActivityA.kt
+++ app/src/main/java/com/example/mycardapp/ActivityA.kt
@@ -1,7 +1,12 @@
 package com.example.mycardapp
 
+import android.content.Intent
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class ActivityA : AppCompatActivity(R.layout.activity_a) {
 
+    fun onOpenBButtonClick(view: View) {
+        startActivity(Intent(this, ActivityB::class.java))
+    }
 }
---
+++ app/src/main/java/com/example/mycardapp/ActivityB.kt
@@ -0,0 +1,7 @@
+package com.example.mycardapp
+
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityB : AppCompatActivity(R.layout.activity_b) {
+
+}

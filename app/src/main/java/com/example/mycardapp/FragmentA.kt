d33f49e1e1e833897cccf16038e8856c93a822bf
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2020-05-11 16:33:06 +0200

    Add Fragment with backstack

--- app/src/main/java/com/example/mycardapp/FragmentA.kt
+++ app/src/main/java/com/example/mycardapp/FragmentA.kt
@@ -1,8 +1,21 @@
 package com.example.mycardapp
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 
 class FragmentA : Fragment(R.layout.fragment_a) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        view.findViewById<Button>(R.id.buttonA).setOnClickListener {
+            parentFragmentManager.commit {
+                replace(R.id.fragmentContainer, FragmentB())
+                addToBackStack(null)
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/example/mycardapp/FragmentB.kt
@@ -0,0 +1,8 @@
+package com.example.mycardapp
+
+import androidx.fragment.app.Fragment
+
+class FragmentB : Fragment(R.layout.fragment_b) {
+
+
+}

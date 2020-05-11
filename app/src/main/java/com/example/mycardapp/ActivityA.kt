4bf81f9fb45810bd42ddc64831b1ca2e0a22b4d9
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2020-05-11 02:49:54 +0200

    Add flags & launch mode

--- app/src/main/java/com/example/mycardapp/ActivityA.kt
+++ app/src/main/java/com/example/mycardapp/ActivityA.kt
@@ -7,6 +7,10 @@ import androidx.appcompat.app.AppCompatActivity
 class ActivityA : AppCompatActivity(R.layout.activity_a) {
 
     fun onOpenBButtonClick(view: View) {
-        startActivity(Intent(this, ActivityB::class.java))
+        val intent = Intent(this, ActivityB::class.java)
+            .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        println(intent.flags and Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/mycardapp/ActivityB.kt
+++ app/src/main/java/com/example/mycardapp/ActivityB.kt
@@ -1,7 +1,20 @@
 package com.example.mycardapp
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class ActivityB : AppCompatActivity(R.layout.activity_b) {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        findViewById<TextView>(R.id.label).text = "${Random.nextInt()}"
+    }
+
+    fun onOpenBAgainButtonClick(view: View) {
+        startActivity(Intent(this, this::class.java))
+    }
 }

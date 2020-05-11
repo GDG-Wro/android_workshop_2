832fcd213cb633757fa42bf6556f157f038480bc
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2020-05-11 03:20:10 +0200

    Add intent action send

--- app/src/main/java/com/example/mycardapp/ActivityA.kt
+++ app/src/main/java/com/example/mycardapp/ActivityA.kt
@@ -1,15 +1,15 @@
 package com.example.mycardapp
 
 import android.content.Intent
-import android.net.Uri
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class ActivityA : AppCompatActivity(R.layout.activity_a) {
 
     fun onOpenBButtonClick(view: View) {
-        val intent = Intent(Intent.ACTION_VIEW)
-            .setData(Uri.parse("https://google.pl"))
-        startActivity(intent)
+        val intent = Intent(Intent.ACTION_SEND)
+            .setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT, "Webinar GDG")
+        startActivity(Intent.createChooser(intent, "Wybierz apkę"))
     }
 }
